8c4c46e8dca049bba8a93e7e57819d079370f24f
crrojas88 <carlorrojas@gmail.com>
2021-02-03 15:13:15 -0800

    added custom button component and style

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './form-input.styles.scss';
-
+// making a separate component for reusability in case we need another form to style.
 const FormInput = ({ handleChange, label, ...otherProps }) => {
     return(
         <div className='group'>
