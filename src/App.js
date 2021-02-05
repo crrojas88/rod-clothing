fe775bde8bccf6319f7dbd2cbae3b26af25ca1c7
crrojas88 <carlorrojas@gmail.com>
2021-02-04 17:01:45 -0800

    implemented functionality to create new users and persist to firebase db

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ componentDidMount() {
             ...snapShot.data()
           }
         })
+        console.log(this.state)
       })
     } else {
       this.setState({currentUser: userAuth})
