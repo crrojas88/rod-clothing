532ddeac7aa5d9b0d8a18371ecffa16b0c752c78
crrojas88 <carlorrojas@gmail.com>
2021-02-04 16:10:43 -0800

    persisted user auth to firebase and state when logging in

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,33 @@ const config = {
     appId: "1:1086586461704:web:cdab09050e458a9b4e3bbd"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    
+    if (!userAuth) return
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get()
+    // checking if snapshot exists, it creates user data in its place.
+    if(!snapShot.exists) {
+      // destructure displayName and email from userAuth object
+      const { displayName, email } = userAuth
+      const createdAt = new Date()
+      try {
+        // create a new user object with the following properties
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (error) {
+        console.log('error creating user')
+      }
+    }
+    return userRef
+  }
+
   firebase.initializeApp(config)
 
   export const auth = firebase.auth()
