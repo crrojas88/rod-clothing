1b51652f1571ea4d8b08f8f7e8d5aa1cb19bc31e
crrojas88 <carlorrojas@gmail.com>
2021-02-12 15:29:22 -0800

    added custom button to collection items, added styling effects to custom button and collection items

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => {
     return(
         // conditionally rendering class of google-sign-in if isGoogleSignIn is true otherwise rendering custom-button.
-        <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+        <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
             {children}
         </button>
     )
