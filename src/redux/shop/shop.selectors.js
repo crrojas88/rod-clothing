7d772d3ade698d806f53f5db80a8efb86a511153
crrojas88 <carlorrojas@gmail.com>
2021-02-25 15:56:42 -0800

    created respective selectors and updated components with new redux flow for directory/shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect';
+// import memoize from 'lodash.memoize';
 
 const selectShop = state => state.shop
 
@@ -6,3 +7,15 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 )
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+// map over collections by selecting it, and passing into selectCollection the collectionUrlParam which is a string and in turn returns createSelector
+export const selectCollection = collectionUrlParam =>
+createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+    )
